c317cb5a9cdb43491d25f28d757fd7df7d424aab
jlake70 <jlake7021@gmail.com>
2024-05-15 12:46:37 -0400

    Corrected task 3 for less than 100 and food choice yes

--- 2. Task 3.py
+++ 2. Task 3.py
@@ -2,4 +2,4 @@ attendees = int(input("Enter number of attendees: "))
 food_choice = input("Do you prefer Vegetarian (yes/no)")
 
 
-print("We recommend large hall with audio system and Veggie Delight Caterers") if attendees > 100 and food_choice == "yes"   else print("We recommend conference hall with projector and Gourmet Meals Caterers") and attendees < 100 and food_choice == "no" 
+print("We recommend large hall with audio system and Veggie Delight Caterers") if attendees > 100 and food_choice == "yes"   else print("We recommend conference hall with projector and Gourmet Meals Caterers") if attendees < 100 and food_choice == "no" else print("We recommend a confernce hall with projector and Veggie Delight Caterers.")
